5f8a25af5ab1d1a3843a10c1cbb12ad626349df1
marimeli <marimel.casas@gmail.com>
2020-02-18 12:42:15 -0500

    SliverPersistentHeader

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:gridview/sliver_appbar.dart';
+import 'package:gridview/sample.dart';
+//import 'package:gridview/sliver_appbar.dart';
 //import 'package:gridview/sliver_grid.dart';
 //import 'sliver_list.dart';
- 
-void main() => runApp(MyApp());
+
+/* void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
@@ -14,7 +15,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyCustomAppBar(),
+      home: MyCustomAppBar(), //Sliver AppBar
+      //home: GridList(), //Sliver Grid
+      //home: DemoPage(), //Sliver List
+    );
+  }
+} */
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+        fontFamily: 'Georgia',
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Collapsing List Demo')),
+        body: CollapsingList(),
+      ),
     );
   }
 }
